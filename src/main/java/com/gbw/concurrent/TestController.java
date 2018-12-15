2f67ef81b696b7d9dd46e04a3dda29d32723b0e2
gbwen <18868111373@163.com>
2018-12-15 10:28:44 +0800

    maven dependency

--- src/main/java/com/gbw/concurrent/TestController.java
+++ src/main/java/com/gbw/concurrent/TestController.java
@@ -1,10 +1,15 @@
 package com.gbw.concurrent;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class TestController {
-
+    @RequestMapping("/test")
+    @ResponseBody
     public String test() {
         return "test";
     }
